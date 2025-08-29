63dce7a7db05e839984ce659dadfacc6f65869a4
DmitryShilov <107641031+DmitryShilov@users.noreply.github.com>
2025-08-29 15:12:08 +0700

    Update ConverterTest.java

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-class ConverterTest {
+public class Converter {
 
     @Test
     void whenConvert270RblThen3Euro() {
