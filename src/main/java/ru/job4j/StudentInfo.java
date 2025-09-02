4fd9420948fcc5ffa9abccd7982fdf7e879ad74e
DmitryShilov <107641031+DmitryShilov@users.noreply.github.com>
2025-09-02 13:35:24 +0700

    Update StudentInfo.java

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("Dmitry Shilov");
         System.out.println("04.11.1985");
     }
