650ea3c5d38158a86cf71a94feb897591df16f1f
Win10 <1985d@inbox.ru>
2025-09-05 00:30:12 +0700

    Тестирование Класса Калькулятора Температуры Хранения [#505279]

---
+++ src/main/java/ru/job4j/calculator/TemperatureFit.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+public class TemperatureFit {
+
+    public static double idealTemperatureFourFruits(int temperature) {
+        return temperature - 2.0;
+    }
+
+    public static double idealTemperatureFromMeat(int temperature) {
+        return temperature - 5.0;
+    }
+
+    public static void main(String[] args) {
+        int temperature = 10;
+        double fruitsTemp = TemperatureFit.idealTemperatureFourFruits(temperature);
+        double meatTemp = TemperatureFit.idealTemperatureFromMeat(temperature);
+        System.out.println("Ideal tempetrature for fruits is " + fruitsTemp + "°C");
+        System.out.println("Ideal temperature from meat is " + meatTemp + "°C");
+
+
+    }
+
+}
